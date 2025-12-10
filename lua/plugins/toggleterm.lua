d1bf7c7228d0bee78c99e90607184721124b5b55
zwhan <hanthaiduong_hsgs2022@hus.edu.vn>
2025-12-10 17:07:46 +0700

    Added toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		opts = {
+			size = 60,
+			open_mapping = false,
+			direction = "vertical",
+			close_on_exit = true,
+			persist_size = true,
+			float_opts = {
+				border = "curved",
+				width = 100,
+				height = 70,
+			},
+		},
+		config = function(_, opts)
+			require("toggleterm").setup(opts)
+
+			function close_term()
+				vim.api.nvim_input("<C-\\><C-n>")
+				vim.cmd("ToggleTerm")
+			end
+
+			vim.keymap.set("n", "<leader>t", "<CMD>ToggleTerm<CR>")
+			vim.keymap.set("t", "<Esc><Esc>", close_term) -- Escape and Close Terminal
+		end,
+	},
+}
