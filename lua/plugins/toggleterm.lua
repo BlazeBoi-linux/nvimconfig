38629f23b204940bb7230427fd4efa454daa029b
zwhan <hanthaiduong_hsgs2022@hus.edu.vn>
2025-12-15 16:10:23 +0700

    Added compilation keymap for C++

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -12,12 +12,35 @@ return {
 		config = function(_, opts)
 			require("toggleterm").setup(opts)
 
-			function close_term()
+			local function close_term()
 				vim.api.nvim_input("<C-\\><C-n>")
 				vim.cmd("ToggleTerm")
 			end
 
-			--== Toggle Terminal
+			local Terminal = require("toggleterm.terminal").Terminal
+
+			-- Compile and Run C++ file
+			vim.keymap.set("n", "<F5>", function()
+				if vim.bo.filetype == "cpp" then
+					local current_file = vim.fn.expand("%: t") -- Gets "main.cpp"
+					local basename = vim.fn.expand("%:t:r") -- Gets "main"
+					local file_dir = vim.fn.expand("%:p:h") -- Gets directory
+
+					local cmd =
+						string.format("g++ -std=c++17 -O2 -Wall %s -o %s && ./%s", current_file, basename, basename)
+
+					local cpp_terminal = Terminal:new({
+						cmd = cmd,
+						dir = file_dir, -- Set working directory to file's directory
+						direction = "vertical",
+						size = 70,
+						close_on_exit = false,
+					})
+					cpp_terminal:toggle()
+				end
+			end, { desc = "CP:  Compile and Run" })
+
+			--== Toggle Terminal Keymaps
 			vim.keymap.set("n", "<leader>t", "<CMD>ToggleTerm<CR>")
 			vim.keymap.set("t", "<Esc><Esc>", close_term) -- Escape and Close Terminal
 		end,
